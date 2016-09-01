44ea89e6bc98c568cf3b74e089c2d80fb5c01188
Nate Biessener <nathaniel.biessener@gmail.com>
2016-08-31 22:53:06 -0500

    cleanup. done for real now

--- scripts/script.js
+++ scripts/script.js
@@ -5,11 +5,13 @@ var savedResults = [];
 $(document).ready(function () {
   console.log('jquery works');
   $('#searchButton').on('click', function () {
-    // console.log('clicked');
+    //make alias for readability
     var movie = $('#movieIn').val();
+    //clear search field
+    $('#movieIn').val('');
+    //if search is blank, inform user
     if (movie === '') {
-      $('#outDiv').empty();
-      $('#outDiv').append('<h1>That\'s like, not a search, man.</h1>');
+      $('#outDiv').prepend('<p id="noSearchMessage">That\'s like, not a search, man.</p>')
     }//end if
     else {
       console.log('searching for', movie);
@@ -18,6 +20,10 @@ $(document).ready(function () {
         url: searchURL,
         dataType: 'JSON',
         success: function(data){
+          if ($('#noSearchMessage')) {
+            $('#noSearchMessage').remove();
+          }
+          //add most recent results to the 'front' of the array
           savedResults.unshift(data.Search);
           displayResults(savedResults);
         },// end success func
@@ -26,46 +32,43 @@ $(document).ready(function () {
   });//end searchButton on click
 });//end doc ready
 
+//function takes an array of search arrays and adds them to the DOM
 var displayResults = function(resultArray){
-  // var placeholder = $('#outDiv').html();
-  // $('#outDiv').empty();
-  // if (savedResults.length > 1) {
-  //   $('#outDiv').append(placeholder);
-  // }
-
   var movieOut = '';
-  for (var j = 0; j < resultArray[0].length; j++){
-    var movie = resultArray[0][j];
-    movieOut += '<div class="movie" id="' + 0 + j + '" style="display:none;margin:2vw;"><h2 style="width:150px;">' + movie.Title + '</h2><p>' +  movie.Year + '</p><button class="removalButton" onclick="removeMovie(' + 0 + ',' + j + ')" style="margin:2px;">Remove this movie</button><br /><img style="height:200px;width:150px;" src="' + movie.Poster + '" alt="Image not found"/></div>';
-  }//end nested for
-
+  //create html for newest array
+  for (var i = 0; i < resultArray[0].length; i++){
+    var movie = resultArray[0][i];
+    movieOut += '<div class="movie" id="' + 0 + i + '" style="display:none;margin:2vw;"><h2 style="width:150px;">' + movie.Title + '</h2><p>' +  movie.Year + '</p><button class="removalButton" onclick="removeMovie(' + 0 + ',' + i + ')" style="margin:2px;">Remove this movie</button><br /><img style="height:200px;width:150px;" src="' + movie.Poster + '" alt="Image not found"/></div>';
+  }//end for
+  //add newest movies to outDiv
   $('#outDiv').prepend(movieOut);
   updateIndices();
+  //fadeIn each new movie sequentially
   $('.movie').each(function(movie){
     $(this).delay(500*(movie+1)).fadeIn(1000);
   });
 };//end displayResults
 
-//function takes two nums, index1, index2.   Will
+//function takes two nums, index1, index2. Removes movie from savedResults[index1][index2] and from DOM.
 var removeMovie = function(movieArray, movie){
-  console.log('in removeMovie with:', movieArray, movie);
-  console.log(savedResults);
+  //remove targeted movie from it's array
   savedResults[movieArray].splice(movie, 1);
+  //fadeOut targeted movie
   $('#' + movieArray + movie).fadeOut(400,'swing',function(){
-    //after fadeout, remove from DOM and update indices of the div ID and removeMovie arguments
+    //after fadeOut, remove from DOM and update indices of the div ID and removeMovie arguments
     $(this).remove();
     updateIndices();
   });//end fadeOut
-  console.log('savedResults after removeMovie:', savedResults);
 };//end removeMovie
 
+//this one's inelegant, but it works
 //updates id's for movie divs and updates removeMovie arguments for each button
 var updateIndices = function(){
-  console.log('in updateButtons');
   //get top level array length and each sub array length
   var topLength = savedResults.map(function(index){
     return index.length;
   });
+  //set counts
   var count1 = 0;
   var count2 = 0;
   //get all movie divs by class and adjust their id's
@@ -93,9 +96,3 @@ var updateIndices = function(){
     }
   });//end removalButton.each callback
 };//end updateButtons
-
-//relevant properties from data.Search:
-//Poster
-//Title
-//Type (movie, game, etc)
-//Year
